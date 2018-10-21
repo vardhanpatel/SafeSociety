f2c3d15faba54e78462f4d6946ac2b23be35c995
Vardhan Patel <vardhanpatel99@gmail.com>
2018-10-21 12:24:20 -0400

    Update run.py

--- Program/run.py
+++ Program/run.py
@@ -5,10 +5,10 @@ import random
 import tweepy
 #from our keys module (keys.py), import the keys dictionary
 
-consumer_key = 'u8nYArqQRb4aCJZmTWI0Co89E'
-consumer_secret = 'YQznDMRpIB1FckvWbBxlojHo2fWHslIeCi1ZTnrlt5tRsks7OR'
-access_token = '3692376076-O70WmkYEDhx9ZLeZiFNJdpr8zd9e8jO119QkPzH'
-access_token_secret = 'G4NdyJfM9eiPVNoNjxvpI9nKFGsHGGOdsacwTUXHKvN9Z'
+consumer_key = 'YOURKEY'
+consumer_secret = 'YOURKEY'
+access_token = 'YOURKEY'
+access_token_secret = 'YOURKEY'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
